8733df81bb08b7a35fc56402c249afa6a3c07d3f
Tyler Steffenhagen <steffenhagen.tyler@gmail.com>
2017-11-11 09:28:44 -0600

    working prototype

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 $(document).ready(onReady);
-
+let sum = 0.0;
 function onReady() {
     console.log('jQuery sourced');
-    
+    $('body').on('click', 'button', submitForm);
 }
-console.log('script.js sourced');
+
+function submitForm () {
+    
+    //Adds form fields to the table
+    $('table').append(`<tr><td>${$('.firstName').val()}</td>
+    <td>${$('.lastName').val()}</td>
+    <td>${$('.employeeID').val()}</td>
+    <td>${$('.jobTitle').val()}</td>
+    <td class:"sal">${$('.annualSalary').val()}</td></tr>`);
+    
+    //Adds salary to Total Salary 
+    $('h2').replaceWith(`<h2>${(sum += parseInt($('.annualSalary').val())) / 12} </h2>`);
+    
+    //Clears the form
+    $('form')[0].reset();
+}
